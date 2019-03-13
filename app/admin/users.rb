6d0d49ee38a33c4a9090c8c820a8ec7d00a49624
ottyann326 <ottyann326@gmail.com>
2019-03-13 18:36:02 +0900

    update_user_index

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,3 +1,18 @@
 ActiveAdmin.register User do
   permit_params :name, :age, :career, :senkei
+
+  index do
+    selectable_column
+    id_column
+    column :name
+    column :age
+    column :career
+    column :senkei
+    column 'ラケット所持数' do |user|
+      user.rackets.count
+    end
+    column :created_at
+    column :updated_at
+    actions
+  end
 end
